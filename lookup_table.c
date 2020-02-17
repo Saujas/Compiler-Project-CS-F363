c390917c08b1584db97a19ce0216deb0ff8f271f
rohitrajhans <rohitrajhans07@gmail.com>
2020-02-17 16:41:50 +0530

    created lookup table

--- lookup_table.c
+++ lookup_table.c
@@ -1,2 +1,93 @@
 #include"lookup_table.h"
 
+lookup_table initialize_lookup_table(int no_of_slots) {
+    lookup_table table;
+    table.no_of_slots = no_of_slots;
+
+    table.slots = (List* ) malloc(sizeof(List) *  no_of_slots);
+    int i;
+    for(i = 0; i<no_of_slots; i++) {
+        table.slots[i] = NULL;
+    }
+    
+    populate_table(table);
+
+    return table;
+}
+
+int hash(char* str, int no_of_slots) {
+    int hash = 0;
+    int i;
+
+    for(i=0; i<strlen(str); i++)
+        hash = (hash + str[i]) % no_of_slots;
+
+    return hash % no_of_slots;
+}
+
+List search(char* id, lookup_table table) {
+    int h = hash(id, table.no_of_slots);
+    List item = table.slots[h];
+
+    while(item != NULL) {
+        if( strcmp(item->lexeme, id) == 0) {
+            return item;
+        }   
+        item = item->next;
+    }
+
+    return NULL;
+}
+
+int add_item(char* token_name, char* lexeme, lookup_table table) {
+    int h = hash(lexeme, table.no_of_slots);
+
+    List l = (List) malloc(sizeof(Node));
+    strcpy(l->token_name, token_name);
+    strcpy(l->lexeme, lexeme);
+    l->next = NULL;
+
+    List item = table.slots[h];
+
+    if(item == NULL) {
+        table.slots[h] = l;
+    }
+    else {
+        while((item->next) != NULL) {
+            item = item->next;    
+        }
+
+        item->next = l;
+    }
+
+    return 1;
+}
+
+int populate_table(lookup_table table) {
+    int i;
+    for(i=0; i<TOKEN_NUMBERS; i++) {
+        add_item(tokens[i], patterns[i], table);
+    }
+
+    return 1;
+}
+
+int print_table(lookup_table table) {
+    int i;
+    List item;
+    for(i=0; i<table.no_of_slots; i++) {
+        item = table.slots[i];
+        while(item != NULL) {
+            printf("Token: %s, Lexeme: %s\n", item->token_name, item->lexeme);
+            item = item->next;
+        }
+    }
+
+    return 1;
+}
+
+// int main() {
+//     lookup_table table = initialize_lookup_table(41);
+//     print_table(table);
+//     return 0;
+// }
--- lookup_table.h
+++ lookup_table.h
@@ -2,10 +2,22 @@
 #include<stdlib.h>
 #include<string.h>
 #define MAX_TOKEN_SIZE 30
+#define TOKEN_NUMBERS 30
+
+
+char * patterns[TOKEN_NUMBERS] = {"integer","real","boolean","of", "array","start",
+                "end","declare","module","driver","program", "get_value",
+                "print","use","with","parameters","true", "false","takes","input","returns",
+                "AND","OR","for", "in", "switch","case","break","default","while"};
+
+char * tokens[TOKEN_NUMBERS] = {"INTEGER","REAL","BOOLEAN","OF","ARRAY","START",
+                        "END","DECLARE","MODULE","DRIVER","PROGRAM","GET_VALUE", "PRINT",
+                        "USE","WITH","PARAMETERS","TRUE","FALSE","TAKES","INPUT","RETURNS",
+                        "AND","OR","FOR","IN", "SWITCH", "CASE","BREAK","DEFAULT","WHILE"};
 
 struct node {
-    char* tokenName[MAX_TOKEN_SIZE];
-    char* lexeme[MAX_TOKEN_SIZE];
+    char token_name[MAX_TOKEN_SIZE];
+    char lexeme[MAX_TOKEN_SIZE];
     struct node* next;
 };
 
@@ -20,3 +32,8 @@ struct lookup {
 
 typedef struct lookup lookup_table;
 
+lookup_table initialize_lookup_table(int no_of_slots);
+int hash(char* str, int no_of_slots);
+List search(char* id, lookup_table table);
+int add_item(char* token_name, char* lexeme, lookup_table table);
+int populate_table(lookup_table table);
